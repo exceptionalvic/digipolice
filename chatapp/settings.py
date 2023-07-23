327efbe91e1033732a66946fbed109bd22f20257
Victor <igbokwev@gmail.com>
2023-07-23 21:28:40 +0100

    added packages to requirements file

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -11,16 +11,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+try:
+    '''load local dev env file'''
+    load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
+except:
+    '''try to load Docker Staging env file'''
+    load_dotenv(os.path.join(BASE_DIR, '.env'))
+else:
+    '''load production env file'''
+    load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bj&^j@h3*n6yk2x=(h))fp39)qqmm^eh7u%&3y7i1esdf1$zkd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
